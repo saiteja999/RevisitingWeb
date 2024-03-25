4e0266b36c698aed96fca9eba19c4c31b6934ded
info2214 <info@cric-indo.com>
2024-03-25 19:41:53 -0400

    ScopeChain

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-console.log(x);         //Hoisting - can access x even before initialization - undefined
-console.log(printHello) //prints func code
-console.log(getName)   //undefined
-console.log(getName1) //undefined
+/*
+scope of var b - where I can access this var b
+Is b inside scope of a xyz function
+*/
 
-var x = 2;
-
-function printHello()
-{
-    console.log('Hello')   //place debug point here to check this func in execution context
+function a(){           //a is lexically inside global scope
+    var b =10;
+    c()                 // c is lexically inside a 
+    function c()        //closure of c is a(lexical scope)
+    {
+        var z = 20
+        console.log(b)
+    }
 }
 
-var getName = function(){   //function expression - behaves as variable and not as function - so prints undefined
-    console.log("Prints name")
-}
-
-var getName1 = () => {
-    console.log("Prints name1");
-}
-
-printHello()   //Brings printHello to callstack at the time of excecution 
+a()
